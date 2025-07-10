057a89a36f468f96c99105cdcb4202bbc72f6348
ali.pourfereydoon <pourfereydoonali@gmail.com>
2025-07-10 08:18:23 -0700

    if user is not login therfore it should not acsess to the some pages an in this secsion we create this subject

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render,redirect,get_object_or_404
 from django.views import View
 from product.models import Product
@@ -10,7 +11,7 @@ from django.http import HttpResponse
 from account.models import Address
 
 
-class CartDetailView(View):
+class CartDetailView(LoginRequiredMixin,View):
     def get(self,request):
         cart = Cart(request)
         return render(request,'cart/cart_detail.html',{'cart':cart})
@@ -31,13 +32,13 @@ class CartDeleteView(View):
         return redirect('cart:cart_detail')    
     
 
-class OrderDetailView(View):
+class OrderDetailView(LoginRequiredMixin,View):
     def get(self , request , pk):
         order = get_object_or_404(Order,id=pk)
         return render(request,'cart/order_detail.html',{'order':order})
 
 
-class OrderCreationView(View):
+class OrderCreationView(LoginRequiredMixin,View):
     def get(self,request):
         cart = Cart(request)
         order = Order.objects.create(user=request.user , total_price = cart.total())
@@ -46,7 +47,7 @@ class OrderCreationView(View):
         cart.remove_cart()
         return redirect('cart:order_detail', order.id) 
     
-class ApplyDiscountView(View):
+class ApplyDiscountView(LoginRequiredMixin,View):
     def post(self,request,pk):
         order = get_object_or_404(Order,id=pk)
         code = request.POST.get('discount_code')  
@@ -83,6 +84,7 @@ class SendRequestView(View):
         address = get_object_or_404(Address,id=request.POST.get('address'))
         order.address = f'{address.address}-{address.phone}'
         order.save()
+        request.session['order_id'] = str(order.id)
         req_data = {
             "merchant_id": MERCHANT,
             "amount": order.total_price,
@@ -102,5 +104,43 @@ class SendRequestView(View):
             e_message = req.json()['errors']['message']
             return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
 
+class VerifyView(View):
+    def get(self , request):
+        t_status = request.GET.get('Status')
+        t_authority = request.GET['Authority']
+        order_id = request.session['order_id']
+        order = Order.objects.get(id=int(order_id))
+        if request.GET.get('Status') == 'OK':
+            req_header = {"accept": "application/json",
+                        "content-type": "application/json'"}
+            req_data = {
+                "merchant_id": MERCHANT,
+                "amount": order.total_price,
+                "authority": t_authority
+            }
+            req = requests.post(url=ZP_API_VERIFY, data=json.dumps(req_data), headers=req_header)
+            if len(req.json()['errors']) == 0:
+                t_status = req.json()['data']['code']
+                if t_status == 100:
+                    order.is_paid=True
+                    order.save()
+                    return HttpResponse('Transaction success.\nRefID: ' + str(
+                        req.json()['data']['ref_id']
+                    ))
+                elif t_status == 101:
+                    return HttpResponse('Transaction submitted : ' + str(
+                        req.json()['data']['message']
+                    ))
+                else:
+                    return HttpResponse('Transaction failed.\nStatus: ' + str(
+                        req.json()['data']['message']
+                    ))
+
+            else:
+                e_code = req.json()['errors']['code']
+                e_message = req.json()['errors']['message']
+                return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
+        else:
+            return HttpResponse('Transaction failed or canceled by user')
 
 
